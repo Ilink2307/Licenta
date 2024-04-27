07d4473190082e2b9a40e8cd54477fa04b4289ab
Ilinca Dumitrascu <ilinca.dumitrascu@gmail.com>
2024-04-27 21:06:26 +0300

    realized i had more animations to do :)

--- QuantumEscape/Assets/Scripts/PlayerMovement.cs
+++ QuantumEscape/Assets/Scripts/PlayerMovement.cs
@@ -15,11 +15,14 @@ public class PlayerMovement : MonoBehaviour
     private WallCollider leftCollider;
     private WallCollider rightCollider;
 
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         transform = GetComponent<Transform>();
         colliders = gameObject.transform.Find("WallColliders").gameObject;
+        animator = GetComponent<Animator>();
 
         upCollider = colliders.transform.Find("WallColliderUp").GetComponent<WallCollider>();
         downCollider = colliders.transform.Find("WallColliderDown").GetComponent<WallCollider>();
@@ -32,30 +35,52 @@ public class PlayerMovement : MonoBehaviour
     {
         if(currentCooldownMovement<=0)
         {
+            animator.SetBool("isMoving", false);
             if (Input.GetButton("Up") && !upCollider.foundWall)
             {
                 transform.position = new Vector3(transform.position.x, transform.position.y + unitMovement, transform.position.z);
                 currentCooldownMovement = cooldownMovement;
+                ResetMoving();
+                animator.SetBool("isMovingUp", true);
+                animator.SetBool("isMoving", true);
             }
             if (Input.GetButton("Down") && !downCollider.foundWall)
             {
                 transform.position = new Vector3(transform.position.x, transform.position.y - unitMovement, transform.position.z);
                 currentCooldownMovement = cooldownMovement;
+                ResetMoving();
+                animator.SetBool("isMovingDown", true);
+                animator.SetBool("isMoving", true);
             }
             if (Input.GetButton("Left") && !leftCollider.foundWall)
             {
                 transform.position = new Vector3(transform.position.x - unitMovement, transform.position.y, transform.position.z);
                 currentCooldownMovement = cooldownMovement;
+                ResetMoving();
+                animator.SetBool("isMovingLeft", true);
+                animator.SetBool("isMoving", true);
             }
             if (Input.GetButton("Right") && !rightCollider.foundWall)
             {
                 transform.position = new Vector3(transform.position.x + unitMovement, transform.position.y, transform.position.z);
                 currentCooldownMovement = cooldownMovement;
+                ResetMoving();
+                animator.SetBool("isMovingRight", true);
+                animator.SetBool("isMoving", true);
             }
         }
         else
         {
             currentCooldownMovement -= Time.deltaTime;
         }
+    }
+
+    void ResetMoving()
+    {
+        animator.SetBool("isMovingDown", false);
+        animator.SetBool("isMovingUp", false);
+        animator.SetBool("isMovingLeft", false);
+        animator.SetBool("isMovingRight", false);
+        animator.SetBool("isMoving", false);
     }
 }
